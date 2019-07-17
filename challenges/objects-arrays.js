e016ef17e3bffb40e153f415eeefc2998cb50932
Your name <a.m.verdine@gmail.com>
2019-07-16 22:17:25 -0400

    Update objects-arays.js with new uni[] syntax

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -116,11 +116,9 @@ console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = [];
-
-graduates.filter((graduates) => {
-  let university = graduates.university;
-  uni.push(graduates[university.search('Uni')]);
+const uni = graduates.filter((graduates) => {
+    let university = graduates.university;
+    return university.includes('Uni');  
 
 
   })
